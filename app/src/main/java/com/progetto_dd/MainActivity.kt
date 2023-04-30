4b7232057cdd3daba08b350762635d46a9724b53
Federico Staffolani <S1097620@studenti.univpm.it>
2023-04-30 17:35:51 +0200

    Aggiunta vista mail utente e foto profilo, aggiunta schermata Profilo

--- app/src/main/java/com/progetto_dd/MainActivity.kt
+++ app/src/main/java/com/progetto_dd/MainActivity.kt
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
+import android.widget.TextView
 import androidx.navigation.findNavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.AppBarConfiguration
@@ -13,28 +14,48 @@ import androidx.drawerlayout.widget.DrawerLayout
 import androidx.navigation.ui.NavigationUI
 import com.google.android.material.navigation.NavigationView
 import com.google.android.material.appbar.MaterialToolbar
+import com.google.firebase.auth.FirebaseAuth
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var firebaseAuth: FirebaseAuth
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        firebaseAuth = FirebaseAuth.getInstance()
 
         // Imposta la toolbar personalizzata
         val toolbar = findViewById<MaterialToolbar>(R.id.toolbar)
         setSupportActionBar(toolbar)
 
+        // Configurazione del controller per navigare tra i fragment
         val navHostFragment = supportFragmentManager
             .findFragmentById(R.id.nav_host_fragment) as NavHostFragment
         val navController = navHostFragment.navController
+
+        // Imposta il drawer
         val drawer = findViewById<DrawerLayout>(R.id.drawer_layout)
         val builder = AppBarConfiguration.Builder(navController.graph)
         builder.setOpenableLayout(drawer)
+
+        // Abilita la navigazione quando un oggetto è cliccato sulla toolbar
         val appBarConfiguration = builder.build()
         toolbar.setupWithNavController(navController, appBarConfiguration)
 
         // Abilita la navigazione quando un oggetto è cliccato sul drawer
         val navView = findViewById<NavigationView>(R.id.nav_view)
         NavigationUI.setupWithNavController(navView, navController)
+
+        // Prende un riferimento alla textview nel navigation header
+        // per far visualizzare sotto all'immagine del profilo la mail dell'utente
+        val headerView = navView.getHeaderView(0)
+        val mailTextView = headerView.findViewById<TextView>(R.id.mail_profilo)
+        val user = firebaseAuth.currentUser
+        if (user != null) {
+            val email = user.email
+            mailTextView.text = email
+        }
     }
 
     // Aggiunge gli item del menu alla toolbar
