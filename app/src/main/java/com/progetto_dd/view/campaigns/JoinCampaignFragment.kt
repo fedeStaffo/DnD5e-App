300cebb6adb65095ea6547f2d1f06008228fa71f
Federico Staffolani <S1097620@studenti.univpm.it>
2023-06-01 10:09:28 +0200

    App finita, forse davvero

--- app/src/main/java/com/progetto_dd/view/campaigns/JoinCampaignFragment.kt
+++ app/src/main/java/com/progetto_dd/view/campaigns/JoinCampaignFragment.kt
@@ -1,16 +1,14 @@
 package com.progetto_dd.view.campaigns
 
-import android.content.Intent
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ArrayAdapter
 import android.widget.Toast
+import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
-import com.progetto_dd.MainActivity
 import com.progetto_dd.R
 import com.progetto_dd.databinding.FragmentJoinCampaignBinding
 import com.progetto_dd.memory.campagna.CampagnaViewModel
@@ -40,25 +38,34 @@ class JoinCampaignFragment : Fragment() {
         val nomiPersonaggi = viewModel.getNomiPersonaggiUtente()
 
         nomiPersonaggi.observe(viewLifecycleOwner) { listaNomi ->
-            val adapter = ArrayAdapter(requireContext(), android.R.layout.simple_spinner_item, listaNomi)
-            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
-            binding.spinnerPersonaggi.adapter = adapter
 
-            binding.buttonPartecipa.setOnClickListener {
-                val nomePersonaggioSelezionato = binding.spinnerPersonaggi.selectedItem.toString()
-                val nomeCampagna = binding.nomecampagnaEt.text.toString()
-                val passCampagna = binding.editTextPassword.text.toString()
 
-                if(nomePersonaggioSelezionato != "" && nomeCampagna != "" && passCampagna != ""){
-                    // Chiamata alla funzione del view model per aggiungere la campagna al personaggio
-                    viewModel.aggiungiCampagnaAPersonaggio(nomePersonaggioSelezionato,nomeCampagna)
-                    // Chiamata alla funzione del view model per aggiungere il personaggio alla campagna
-                    viewModel.aggiungiPersonaggioACampagna(requireContext(), nomeCampagna, passCampagna, nomePersonaggioSelezionato)
+            if (listaNomi.isEmpty()) {
+                binding.spinnerPersonaggi.visibility = View.GONE
+                Toast.makeText(requireContext(), "Devi prima creare un personaggio prima di unirti ad una campagna", Toast.LENGTH_SHORT).show()
+                binding.buttonPartecipa.visibility = View.GONE
+            }
+            else {
+                val adapter = ArrayAdapter(requireContext(), android.R.layout.simple_spinner_item, listaNomi)
+                adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+                binding.spinnerPersonaggi.adapter = adapter
 
-                    // Riporta alla pagina principale
-                    findNavController().navigate(R.id.action_joinCampaignFragment_to_homeCampaignsFragment)
+                binding.buttonPartecipa.setOnClickListener {
+                    val nomePersonaggioSelezionato = binding.spinnerPersonaggi.selectedItem.toString()
+                    val nomeCampagna = binding.nomecampagnaEt.text.toString()
+                    val passCampagna = binding.editTextPassword.text.toString()
+
+                    if(nomePersonaggioSelezionato != "" && nomeCampagna != "" && passCampagna != ""){
+                        // Chiamata alla funzione del view model per aggiungere la campagna al personaggio
+                        viewModel.aggiungiCampagnaAPersonaggio(nomePersonaggioSelezionato,nomeCampagna)
+                        // Chiamata alla funzione del view model per aggiungere il personaggio alla campagna
+                        viewModel.aggiungiPersonaggioACampagna(requireContext(), nomeCampagna, passCampagna, nomePersonaggioSelezionato)
+
+                        // Riporta alla pagina principale
+                        findNavController().navigate(R.id.action_joinCampaignFragment_to_homeCampaignsFragment)
+                    }
+                    else Toast.makeText(requireContext(), "Non sono ammessi campi vuoti!", Toast.LENGTH_SHORT).show()
                 }
-                else Toast.makeText(requireContext(), "Non sono ammessi campi vuoti!", Toast.LENGTH_SHORT).show()
             }
         }
     }
