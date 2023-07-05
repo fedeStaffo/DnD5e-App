cc356543336a3d08a55eb70c1e293b288b84239e
Ems <emsardellini@gmail.com>
2023-07-05 16:15:39 +0200

    finita la creazione

--- app/src/main/java/com/progetto_dd/view/characters/crea/CompetenzeFragment.kt
+++ app/src/main/java/com/progetto_dd/view/characters/crea/CompetenzeFragment.kt
@@ -39,6 +39,7 @@ class CompetenzeFragment : Fragment() {
         // Ottieni le competenze per una specifica classe (esempio: "Guerriero")
         val classe = viewModelPersonaggio.classePersonaggio.value
         val mezzelfo = viewModelPersonaggio.competenzeMezzelfoPersonaggio.value
+        val razza = viewModelPersonaggio.razzaPersonaggio.value
 
 
         if (classe != null) {
@@ -312,78 +313,83 @@ class CompetenzeFragment : Fragment() {
                     }
                 }
 
-                if (classe == "Bardo") {
-                    if(myList.size != numCompetenze){
+                if(razza != null) {
+                    if (classe == "Bardo") {
+                        if(myList.size != numCompetenze){
+                            Toast.makeText(
+                                context,
+                                "Seleziona il numero richiesto di competenze!",
+                                Toast.LENGTH_LONG
+                            ).show()
+                        }else if (selezione1 == selezione2 || selezione2 == selezione3 || selezione1 == selezione3){
+                            Toast.makeText(
+                                context,
+                                "Le competenze musicali devono essere diverse tra di loro",
+                                Toast.LENGTH_LONG
+                            ).show()
+                        }else if (selezione1 == "Scegli una competenza musicale" || selezione2 == "Scegli una competenza musicale" || selezione3 == "Scegli una competenza musicale"){
+                            Toast.makeText(
+                                context,
+                                "Non hai selezionato tutte le competenze musicali",
+                                Toast.LENGTH_LONG
+                            ).show()
+                        }else{
+                            if (selezione1 != null && selezione2 != null && selezione3 != null) {
+                                myList.add(selezione1)
+                                myList.add(selezione2)
+                                myList.add(selezione3)
+                                if (list != null) myList.addAll(list)
+                            }
+                            if(razza == "Elfo alto" || razza == "Elfo dei boschi" || razza == "Elfo oscuro") myList.add("Percezione")
+                            viewModelPersonaggio.setCompetenzePersonaggio(myList)
+                            findNavController().navigate(R.id.action_competenzeFragment_to_backgroundFragment)
+                        }
+                    }else if (classe == "Monaco"){
+                        if(myList.size != numCompetenze){
+                            Toast.makeText(
+                                context,
+                                "Seleziona il numero richiesto di competenze!",
+                                Toast.LENGTH_LONG
+                            ).show()
+                        }else if (selezioneMus == "Scegli una competenza musicale" && selezioneArt == "Scegli una competenza da artigiano"){
+                            Toast.makeText(
+                                context,
+                                "Devi selezionare una competenza aggiuntiva",
+                                Toast.LENGTH_LONG
+                            ).show()
+                        }else if (selezioneMus != "Scegli una competenza musicale" && selezioneArt != "Scegli una competenza da artigiano"){
+                            Toast.makeText(
+                                context,
+                                "Devi selezionare solo una competenza. Rimetti (Scegli una competenza musicale) o (Scegli una competenza da artigiano) per andare avanti",
+                                Toast.LENGTH_LONG
+                            ).show()
+                        }else {
+                            if (selezioneMus != null && selezioneArt != null) {
+                                if(selezioneMus != "Scegli una competenza musicale") {
+                                    myList.add(selezioneMus)
+                                    if (list != null) myList.addAll(list)
+                                }
+                                else {
+                                    myList.add(selezioneArt)
+                                    if (list != null) myList.addAll(list)
+                                }
+                            }
+                            if(razza == "Elfo alto" || razza == "Elfo dei boschi" || razza == "Elfo oscuro") myList.add("Percezione")
+                            viewModelPersonaggio.setCompetenzePersonaggio(myList)
+                            findNavController().navigate(R.id.action_competenzeFragment_to_backgroundFragment)
+                        }
+                    }else if (myList.size == numCompetenze) {
+                        if (list != null) myList.addAll(list)
+                        if(razza == "Elfo alto" || razza == "Elfo dei boschi" || razza == "Elfo oscuro") myList.add("Percezione")
+                        viewModelPersonaggio.setCompetenzePersonaggio(myList)
+                        findNavController().navigate(R.id.action_competenzeFragment_to_backgroundFragment)
+                    } else {
                         Toast.makeText(
                             context,
                             "Seleziona il numero richiesto di competenze!",
                             Toast.LENGTH_LONG
                         ).show()
-                    }else if (selezione1 == selezione2 || selezione2 == selezione3 || selezione1 == selezione3){
-                        Toast.makeText(
-                            context,
-                            "Le competenze musicali devono essere diverse tra di loro",
-                            Toast.LENGTH_LONG
-                        ).show()
-                    }else if (selezione1 == "Scegli una competenza musicale" || selezione2 == "Scegli una competenza musicale" || selezione3 == "Scegli una competenza musicale"){
-                        Toast.makeText(
-                            context,
-                            "Non hai selezionato tutte le competenze musicali",
-                            Toast.LENGTH_LONG
-                        ).show()
-                    }else{
-                        if (selezione1 != null && selezione2 != null && selezione3 != null) {
-                            myList.add(selezione1)
-                            myList.add(selezione2)
-                            myList.add(selezione3)
-                            if (list != null) myList.addAll(list)
-                        }
-                        viewModelPersonaggio.setCompetenzePersonaggio(myList)
-                        findNavController().navigate(R.id.action_competenzeFragment_to_backgroundFragment)
                     }
-                }else if (classe == "Monaco"){
-                    if(myList.size != numCompetenze){
-                        Toast.makeText(
-                            context,
-                            "Seleziona il numero richiesto di competenze!",
-                            Toast.LENGTH_LONG
-                        ).show()
-                    }else if (selezioneMus == "Scegli una competenza musicale" && selezioneArt == "Scegli una competenza da artigiano"){
-                        Toast.makeText(
-                            context,
-                            "Devi selezionare una competenza aggiuntiva",
-                            Toast.LENGTH_LONG
-                        ).show()
-                    }else if (selezioneMus != "Scegli una competenza musicale" && selezioneArt != "Scegli una competenza da artigiano"){
-                        Toast.makeText(
-                            context,
-                            "Devi selezionare solo una competenza. Rimetti (Scegli una competenza musicale) o (Scegli una competenza da artigiano) per andare avanti",
-                            Toast.LENGTH_LONG
-                        ).show()
-                    }else {
-                        if (selezioneMus != null && selezioneArt != null) {
-                            if(selezioneMus != "Scegli una competenza musicale") {
-                                myList.add(selezioneMus)
-                                if (list != null) myList.addAll(list)
-                            }
-                            else {
-                                myList.add(selezioneArt)
-                                if (list != null) myList.addAll(list)
-                            }
-                        }
-                        viewModelPersonaggio.setCompetenzePersonaggio(myList)
-                        findNavController().navigate(R.id.action_competenzeFragment_to_backgroundFragment)
-                    }
-                }else if (myList.size == numCompetenze) {
-                    if (list != null) myList.addAll(list)
-                    viewModelPersonaggio.setCompetenzePersonaggio(myList)
-                    findNavController().navigate(R.id.action_competenzeFragment_to_backgroundFragment)
-                } else {
-                    Toast.makeText(
-                        context,
-                        "Seleziona il numero richiesto di competenze!",
-                        Toast.LENGTH_LONG
-                    ).show()
                 }
             }
         }
--- app/src/main/java/com/progetto_dd/view/characters/crea/SalvaPersonaggioFragment.kt
+++ app/src/main/java/com/progetto_dd/view/characters/crea/SalvaPersonaggioFragment.kt
@@ -17,6 +17,7 @@ class SalvaPersonaggioFragment : Fragment() {
     private var _binding: FragmentSalvaPersonaggioBinding? = null
     private val binding get() = _binding!!
     private val myListLinguaggi: MutableList<String> = mutableListOf()
+    private val myListCompetenze: MutableList<String> = mutableListOf()
     private lateinit var viewModel: PersonaggioViewModel
 
     override fun onCreateView(
@@ -34,65 +35,188 @@ class SalvaPersonaggioFragment : Fragment() {
         viewModel = ViewModelProvider(requireActivity()).get(PersonaggioViewModel::class.java)
 
         val razza = viewModel.razzaPersonaggio.value
+        val classe = viewModel.classePersonaggio.value
 
-        if (razza != null) {
-            when (razza) {
-                "Umano" -> {
-                    myListLinguaggi.add("Comune")
-                }
-                "Elfo alto", "Elfo dei boschi", "Elfo oscuro" -> {
-                    myListLinguaggi.add("Comune")
-                    myListLinguaggi.add("Elfico")
-                }
-                "Nano delle colline", "Nano delle montagne" -> {
-                    myListLinguaggi.add("Comune")
-                    myListLinguaggi.add("Nanico")
-                }
-                "Halfling piedelesto", "Halfling tozzo" -> {
-                    myListLinguaggi.add("Comune")
-                    myListLinguaggi.add("Halfling")
-                }
-                "Gnomo delle rocce", "Gnomo delle foreste" -> {
-                    myListLinguaggi.add("Comune")
-                    myListLinguaggi.add("Gnomesco")
-                }
-                "Mezzorco" -> {
-                    myListLinguaggi.add("Comune")
-                    myListLinguaggi.add("Orchesco")
-                }
-                "Tiefling" -> {
-                    myListLinguaggi.add("Comune")
-                    myListLinguaggi.add("Infernale")
-                }
-                "Mezzelfo" -> {
-                    myListLinguaggi.add("Comune")
-                    myListLinguaggi.add("Elfico")
-                }
-                "Dragonide d'argento", "Dragonide bianco", "Dragonide blu", "Dragonide di bronzo", "Dragonide nero", "Dragonide d'oro", "Dragonide d'ottone", "Dragonide di rame", "Dragonide rosso", "Dragonide verde" -> {
-                    myListLinguaggi.add("Comune")
-                    myListLinguaggi.add("Draconico")
-                }
+        // Salva il personaggio
+        binding.btnSalva.setOnClickListener {
+
+            if (razza != null && classe != null) {
+                getManuale(razza, classe)
             }
+            val listLinguaggi = viewModel.linguaggiPersonaggio.value
+            if (listLinguaggi != null) myListLinguaggi.addAll(listLinguaggi)
+            viewModel.setLinguaggiPersonaggio(myListLinguaggi)
 
-            // Salva il personaggio
-            binding.btnSalva.setOnClickListener {
+            val listCompetenze = viewModel.competenzePersonaggio.value
+            if(listCompetenze != null) myListCompetenze.addAll(listCompetenze)
+            viewModel.setCompetenzePersonaggio(myListCompetenze)
 
-                val listLinguaggi = viewModel.linguaggiPersonaggio.value
-                if (listLinguaggi != null) myListLinguaggi.addAll(listLinguaggi)
-                viewModel.setLinguaggiPersonaggio(myListLinguaggi)
+            viewModel.saveCharacterToFirestore()
 
-                viewModel.saveCharacterToFirestore()
-
-                // Restarta l'activity personaggio
-                val intent = Intent(activity, CharacterActivity::class.java)
-                startActivity(intent)
-                activity?.finish()
-            }
+            // Restarta l'activity personaggio
+            val intent = Intent(activity, CharacterActivity::class.java)
+            startActivity(intent)
+            activity?.finish()
         }
+        
     }
 
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+    }
+    
+    private fun getManuale(razza: String, classe: String){
+        when (razza) {
+            "Umano" -> {
+                myListLinguaggi.add("Comune")
+            }
+            "Elfo alto", "Elfo dei boschi" -> {
+                myListLinguaggi.add("Comune")
+                myListLinguaggi.add("Elfico")
+                myListCompetenze.add("Spade corte")
+                myListCompetenze.add("Spade lunghe")
+                myListCompetenze.add("Archi corti")
+                myListCompetenze.add("Archi lunghi")
+            }
+            "Elfo oscuro" ->{
+                myListLinguaggi.add("Comune")
+                myListLinguaggi.add("Elfico")
+                myListCompetenze.add("Spade corte")
+                myListCompetenze.add("Stocchi")
+                myListCompetenze.add("Balestre a mano")
+            }
+            "Nano delle colline" -> {
+                myListLinguaggi.add("Comune")
+                myListLinguaggi.add("Nanico")
+                myListCompetenze.add("Asce")
+                myListCompetenze.add("Asce da battaglia")
+                myListCompetenze.add("Martelli da guerra")
+                myListCompetenze.add("Martelli leggeri")
+            }
+            "Nano delle montagne" -> {
+                myListLinguaggi.add("Comune")
+                myListLinguaggi.add("Nanico")
+                myListCompetenze.add("Asce")
+                myListCompetenze.add("Asce da battaglia")
+                myListCompetenze.add("Martelli da guerra")
+                myListCompetenze.add("Martelli leggeri")
+                myListCompetenze.add("Armature leggere")
+                myListCompetenze.add("Armature medie")
+            }
+            "Halfling piedelesto", "Halfling tozzo" -> {
+                myListLinguaggi.add("Comune")
+                myListLinguaggi.add("Halfling")
+            }
+            "Gnomo delle rocce", "Gnomo delle foreste" -> {
+                myListLinguaggi.add("Comune")
+                myListLinguaggi.add("Gnomesco")
+            }
+            "Mezzorco" -> {
+                myListLinguaggi.add("Comune")
+                myListLinguaggi.add("Orchesco")
+            }
+            "Tiefling" -> {
+                myListLinguaggi.add("Comune")
+                myListLinguaggi.add("Infernale")
+            }
+            "Mezzelfo" -> {
+                myListLinguaggi.add("Comune")
+                myListLinguaggi.add("Elfico")
+            }
+            "Dragonide d'argento", "Dragonide bianco", "Dragonide blu", "Dragonide di bronzo", "Dragonide nero", "Dragonide d'oro", "Dragonide d'ottone", "Dragonide di rame", "Dragonide rosso", "Dragonide verde" -> {
+                myListLinguaggi.add("Comune")
+                myListLinguaggi.add("Draconico")
+            }
+        }
+        when(classe){
+            "Barbaro" -> {
+                myListCompetenze.add("Armature leggere")
+                myListCompetenze.add("Armature medie")
+                myListCompetenze.add("Scudi")
+                myListCompetenze.add("Armi semplici")
+                myListCompetenze.add("Armi da guerra")
+            }
+            "Bardo" -> {
+                myListCompetenze.add("Armature leggere")
+                myListCompetenze.add("Armi semplici")
+                myListCompetenze.add("Balestre a mano")
+                myListCompetenze.add("Spade corte")
+                myListCompetenze.add("Spade lunghe")
+                myListCompetenze.add("Stocchi")
+            }
+            "Chierico" -> {
+                myListCompetenze.add("Armature leggere")
+                myListCompetenze.add("Armature medie")
+                myListCompetenze.add("Scudi")
+                myListCompetenze.add("Armi semplici")
+            }
+            "Druido" -> {
+                myListCompetenze.add("Armature leggere")
+                myListCompetenze.add("Armature medie")
+                myListCompetenze.add("Scudi")
+                myListCompetenze.add("Bastoni ferrati")
+                myListCompetenze.add("Dardi")
+                myListCompetenze.add("Falcetti")
+                myListCompetenze.add("Fionde")
+                myListCompetenze.add("Giavellotti")
+                myListCompetenze.add("Lance")
+                myListCompetenze.add("Mazze")
+                myListCompetenze.add("Pugnali")
+                myListCompetenze.add("Randelli")
+                myListCompetenze.add("Scimitarre")
+                myListCompetenze.add("Strumenti da erborista")
+            }
+            "Guerriero" -> {
+                myListCompetenze.add("Tutte le armature")
+                myListCompetenze.add("Scudi")
+                myListCompetenze.add("Armi semplici")
+                myListCompetenze.add("Armi da guerra")
+            }
+            "Ladro" -> {
+                myListCompetenze.add("Armature leggere")
+                myListCompetenze.add("Armi semplici")
+                myListCompetenze.add("Balestre a mano")
+                myListCompetenze.add("Spade corte")
+                myListCompetenze.add("Spade lunghe")
+                myListCompetenze.add("Stocchi")
+                myListCompetenze.add("Arnesi da scasso")
+            }
+            "Mago" -> {
+                myListCompetenze.add("Balestre leggere")
+                myListCompetenze.add("Bastoni ferrati")
+                myListCompetenze.add("Dardi")
+                myListCompetenze.add("Fionde")
+                myListCompetenze.add("Pugnali")
+            }
+            "Monaco" -> {
+                myListCompetenze.add("Armi semplici")
+                myListCompetenze.add("Spade corte")
+            }
+            "Paladino" -> {
+                myListCompetenze.add("Tutte le armature")
+                myListCompetenze.add("Scudi")
+                myListCompetenze.add("Armi semplici")
+                myListCompetenze.add("Armi da guerra")
+            }
+            "Ranger" -> {
+                myListCompetenze.add("Armature leggere")
+                myListCompetenze.add("Armature medie")
+                myListCompetenze.add("Scudi")
+                myListCompetenze.add("Armi semplici")
+                myListCompetenze.add("Armi da guerra")
+            }
+            "Stregone" -> {
+                myListCompetenze.add("Balestre leggere")
+                myListCompetenze.add("Bastoni ferrati")
+                myListCompetenze.add("Dardi")
+                myListCompetenze.add("Fionde")
+                myListCompetenze.add("Pugnali")
+            }
+            "Warlock" -> {
+                myListCompetenze.add("Armature leggere")
+                myListCompetenze.add("Armi semplici")
+            }
+        }
     }
 }
